9c49edd2e7fe198892cbf85813d48be065629c6f
Administrator <stian@eonbit.com>
2014-10-13 22:36:59 +0200

    Option to showForm

--- superfeedback.js
+++ superfeedback.js
@@ -8,6 +8,10 @@ var SuperFeedbackForm = function(settings) {
     self.attach = function() {
 		self.container = (settings.container) ? $(settings.container) : $('<div id="sfb-form" class="' + self.settings.positionClass + '" style="display: none"></div>').appendTo('body');
         self.container.html(self.render());
+		if (self.settings.showForm)
+			self.container.show();
+		else
+			self.container.hide();
         self.drawContainer      = self.container.find('.sfb-form-drawing-container');
         self.drawInfo           = self.container.find('#sfb-form-drawing-info');
         self.startDrawButton    = self.container.find('#sfb-enable');
